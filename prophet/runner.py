ebc7025be1a5aab6f1a2ccc2f0d67b9ad42aa89a
Raghu Reddy <raghureddy.ram@gmail.com>
2023-12-23 15:02:50 -0800

    Refactor

--- prophet/runner.py
+++ prophet/runner.py
@@ -9,23 +9,26 @@ def parse_args():
     parser.add_argument("--year", type=str, required=True)
     return parser.parse_args()
 
-def start():
-    fiscal_year = parse_args().year
-    if fiscal_year not in ["2022", "2023"]:
-        raise "please restart this program with the --year=XXXX modifier. Currently year may be 2022 or 2023"
+def initialize_chatbot(fiscal_year: str) -> AnswerGenerator:
     load_dotenv()
     openai.api_key = os.getenv("OPENAI_API_KEY")
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     
     faiss_index, pre_processor, paragraphs  = LoadIndex(fiscal_year).load_index()
     encoder_model = pre_processor.encoder_model
-    answer_generator = AnswerGenerator(encoder_model=encoder_model, index=faiss_index, paragraphs=paragraphs)
+    return AnswerGenerator(encoder_model=encoder_model, index=faiss_index, paragraphs=paragraphs)
+
+def start() -> None:
+    fiscal_year = parse_args().year
+    if fiscal_year not in ["2022", "2023"]:
+        raise ValueError("Please restart this program with the --year=XXXX modifier. Currently year may be 2022 or 2023")
+
+    answer_generator = initialize_chatbot(fiscal_year)
 
     is_valid_question = True
     print(f"Welcome to earnings review! This chatbot will allow you to comb through the pdf of Microsoft's annual financial statements for the year {fiscal_year}")
     print("Please ask your questions and I will do my best to answer.")
     
-        
     while is_valid_question:
         question= input("Please ask your question\n\n")
         if question == "exit" or question == "quit":
